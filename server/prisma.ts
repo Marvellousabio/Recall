c51f66668edc30464732289d66232aa6150eab43
NELITE-7 <nelsonnoragbon23@yahoo.com>
2026-07-03 14:47:53 +0100

    n

--- server/prisma.ts
+++ server/prisma.ts
@@ -1,15 +1,3 @@
 import { PrismaClient } from '@prisma/client';
 
-const databaseUrl = process.env.DATABASE_URL;
-
-if (!databaseUrl) {
-  throw new Error('DATABASE_URL environment variable is required');
-}
-
-export const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: databaseUrl,
-    },
-  },
-});
+export const prisma = new PrismaClient();
